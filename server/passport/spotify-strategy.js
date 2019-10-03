38738b0f8ddff39defbf41e007e20bdf9074f2ac
Evan Guirino <etg624@gmail.com>
2019-10-02 22:44:28 -0700

    deploy setup

--- server/passport/spotify-strategy.js
+++ server/passport/spotify-strategy.js
@@ -23,7 +23,10 @@ const spotifyStrategy = new SpotifyStrategy(
     const { displayName, id } = profile;
     const newUser = { displayName, spotifyId: id, accessToken, refreshToken };
     process.nextTick(() => {
-      console.log('heereeeeee', User);
+      console.log(
+        'heereeeeee',
+        User.find({ spotifyId: id }).then(user => user)
+      );
       return User.find({ spotifyId: id })
         .then(user => {
           console.log('HEERREEEE 28', newUser);
